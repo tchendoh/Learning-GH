7698000fd790d6a9980f3dd48b19c4cb069a6b1f
tchendoh <42325308+tchendoh@users.noreply.github.com>
2022-11-20 12:18:07 -0500

    Initial Commit

---
+++ LearningGH/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LearningGH
+//
+//  Created by Eric Chandonnet on 2022-11-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Eric Chandonnet on 2022-11-20.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
